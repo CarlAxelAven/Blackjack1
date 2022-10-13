96151f6daf09eaf76569b72b50e054162e8dff68
Carl Axel <Carl Axel@SCHRDINGER>
2022-10-13 12:37:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string play = "";
+while(play != "3")
+{
+    Console.WriteLine("Skriv siffran för vad du vill göra.");
+    Console.WriteLine("1. Spela Blackjack med datorn.");
+    Console.WriteLine("2. Kolla tidigare vinnare.");
+    Console.WriteLine("3. Avsluta programmet.");
+   switch(play)
+    {
+        case "1": 
+
+    }
+}
